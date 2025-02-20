b5d56de751be8c3087259b851f7da1d9fd198f71
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-21 11:49:30 +1300

    add route handling outline

---
+++ stdlib-basic.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := NewTaskServer()
+
+	mux.HandleFunc("POST /task/", server.createTaskHandler)
+	mux.HandleFunc("GET /task/", server.getAllTasksHandler)
+	mux.HandleFunc("DELETE /task/", server.deleteAllTasksHandler)
+	mux.HandleFunc("GET /task/{id}/", server.getTaskHandler)
+	mux.HandleFunc("DELETE /task/{id}/", server.deleteTaskHandler)
+	mux.HandleFunc("GET /tag/{tag}/", server.tagHandler)
+	mux.HandleFunc("GET /due/{year}/{month}/{day}/", server.dueHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
+}

430502cb0d50f245deae456054b12fc7fccff58a
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-21 11:59:22 +1300

    Add json marshalling

--- stdlib-basic.go
+++ stdlib-basic.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -19,4 +30,29 @@ func main() {
 	mux.HandleFunc("GET /due/{year}/{month}/{day}/", server.dueHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
+}
+
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get task at %s\n", req.URL.path)
+
+	id, err := strconv.Atoi(req.PathValue("id"))
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	task, err := ts.store.GetTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	js, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }

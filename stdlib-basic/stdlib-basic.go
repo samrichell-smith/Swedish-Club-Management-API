17e5b9f22a1e5c491049e5ce0ddfb5f105d9d114
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-21 19:38:13 +1300

    Implement create task handler func

--- stdlib-basic/stdlib-basic.go
+++ stdlib-basic/stdlib-basic.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"stdlib-basic/taskstore"
 )
@@ -32,6 +34,49 @@ func main() {
 	mux.HandleFunc("GET /due/{year}/{month}/{day}/", server.dueHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling task create at %s\n", req.URL.Path)
+
+	type RequestTask struct {
+		Text string `json:"text"`
+		Tags []string `json:"tags"`
+		Due time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	js, err := json.Marshal(ResponseId{Id: id})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
